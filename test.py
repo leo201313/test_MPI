2b43b3a5a233128f5e5881e7095b040749247a41
KRAD <978404740@qq.com>
2019-09-16 20:10:08 +0800

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = [1,2,3]
+    comm.send(data,dest=1,tag=11)
+
+if rank == 1:
+    atad = comm.rec(source=0,tag=11)
+    print(atad)
+
